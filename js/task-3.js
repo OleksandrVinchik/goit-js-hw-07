52ab93372e63ed72aa667c347cf0c76b814664bf
oleksandr <groupdchem.sales@gmail.com>
2024-11-15 19:50:36 +0200

    add initial files

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,16 @@
-function checkForSpam(message) {
-  const normalizedStr = message.toLowerCase();
-  const result =
-    normalizedStr.includes(`spam`) || normalizedStr.includes(`sale`);
-  return result;
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredNumbers.push(numbers[i]);
+    }
+  }
+  return filteredNumbers;
 }
 
-console.log(checkForSpam('Latest technology news')); // false
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
-console.log(checkForSpam('Get best sale offers now!')); // true
-console.log(checkForSpam('Amazing SalE, only tonight!')); // true
-console.log(checkForSpam('Trust me, this is not a spam message')); // true
-console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+// Код для проверки
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
